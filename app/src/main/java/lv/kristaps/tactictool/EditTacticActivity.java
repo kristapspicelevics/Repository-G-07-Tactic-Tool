a1aefdcde3f4014d38c4c3642c2e3dbd09b4198c
abols360 <aleksis.abols@va.lv>
2022-01-03 19:00:19 +0200

    add safty check and other info

--- app/src/main/java/lv/kristaps/tactictool/EditTacticActivity.java
+++ app/src/main/java/lv/kristaps/tactictool/EditTacticActivity.java
@@ -5,25 +5,96 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class EditTacticActivity extends AppCompatActivity {
 
+    String name = " ";
+    String notName= "Bad input";
+
+    Button BSelectImage;
+    ImageView IVPreviewImage;
+    int SELECT_PICTURE = 200;
+    EditText TextTacticName;
+
+    Button saveTacticButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_tactic);
+        BSelectImage = findViewById(R.id.BSelectImage);
+        IVPreviewImage = findViewById(R.id.IVPreviewImage);
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+                //setContentView(R.layout.activity_main);
 
+            }
+        });
+        TextTacticName = (EditText) findViewById(R.id.TextTacticName);
 
+        saveTacticButton = (Button) findViewById(R.id.saveTactic);
+        saveTacticButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {// checko vai ir nosaukums ievadits
+                name= TextTacticName.getText().toString();
+                //name=IVPreviewImage.getTag().toString();
+                if (TextUtils.isEmpty(TextTacticName.getText().toString())){
+                    showToast(notName);
+                }else{
+                    showToast(name);
+                }
 
+            }
+        });
     }
+    private  void showToast(String text){
+        Toast.makeText(EditTacticActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    IVPreviewImage.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+
 
 
     @Override
