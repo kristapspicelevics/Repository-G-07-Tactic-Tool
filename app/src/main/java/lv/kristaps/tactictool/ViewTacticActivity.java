b4470096c5f980176b9f64e6270aa660baeab028
ainars <ainars.berzins@elbune.lv>
2022-01-04 01:36:04 +0200

    pirms pulla

--- app/src/main/java/lv/kristaps/tactictool/ViewTacticActivity.java
+++ app/src/main/java/lv/kristaps/tactictool/ViewTacticActivity.java
@@ -1,19 +1,57 @@
 package lv.kristaps.tactictool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ViewTacticActivity extends AppCompatActivity {
+    private StorageReference mStorageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tactic);
+
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("82a3a940-8869-429e-b06d-014825fb1152");
+
+        try {
+            final File localFile = File.createTempFile("82a3a940-8869-429e-b06d-014825fb1152", "*");
+            mStorageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(ViewTacticActivity.this, "Picture Dowloaded", Toast.LENGTH_SHORT).show();
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    ((ImageView)findViewById(R.id.IVPreviewImage)).setImageBitmap(bitmap);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(ViewTacticActivity.this, "Picture Not Dowloaded", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
